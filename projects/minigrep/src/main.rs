cff6adcf8f74a0a4f9e1aa8b769cae7c6d255830
LeeTrent <lee.trent.1@gmail.com>
2023-03-14 06:03:30 -0500

    First commit

---
+++ projects/minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+
+    let program_name = &args[0];
+    let query = &args[1];
+    let file_path = &args[2];
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("Program name is {}", program_name);
+    println!("Searching for {}", query);
+    println!("In file {}", file_path); 
+    println!("With text\n{contents}");
+}
