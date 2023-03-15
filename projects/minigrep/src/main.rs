7d5d41ac82aa6fd03278b59ff0787b3d655adfcf
LeeTrent <lee.trent.1@gmail.com>
2023-03-15 05:56:19 -0500

    Placed config values in its own struct

--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -3,15 +3,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //dbg!(args);
+    let config = parse_confg(&args);
 
-    let program_name = &args[0];
-    let query = &args[1];
-    let file_path = &args[2];
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path); 
 
-    println!("Program name is {}", program_name);
-    println!("Searching for {}", query);
-    println!("In file {}", file_path); 
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
     println!("With text\n{contents}");
 }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_confg(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config {query, file_path}
+}
+
+
