7245d868cb388a7a90baebd3a1f0545d498e60ea
LeeTrent <lee.trent.1@gmail.com>
2023-04-08 08:54:00 -0500

    Adds mutexes project

---
+++ projects/mutexes/target/debug/deps/mutexes.d
@@ -0,0 +1,5 @@
+C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\target\debug\deps\mutexes.exe: src\main.rs
+
+C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\target\debug\deps\mutexes.d: src\main.rs
+
+src\main.rs:
---
+++ projects/mutexes/target/debug/mutexes.d
@@ -0,0 +1,1 @@
+C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\target\debug\mutexes.exe: C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\src\main.rs
