0d981a805aa6970da5207a5be6c5d75bef22daaf
LeeTrent <lee.trent.1@gmail.com>
2023-04-08 09:02:43 -0500

    Removed the target folder

--- projects/mutexes/target/debug/deps/mutexes.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\target\debug\deps\mutexes.exe: src\main.rs
-
-C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\target\debug\deps\mutexes.d: src\main.rs
-
-src\main.rs:
--- projects/mutexes/target/debug/mutexes.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\target\debug\mutexes.exe: C:\Users\Lee\Dev\Learning\Rust\Repo\rust-book-2023\projects\mutexes\src\main.rs
